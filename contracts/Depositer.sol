8a9c9a91800e1c55d978c8e52b3c948bcf1fce9c
Andy Johnson <andreysima6@gmail.com>
2023-07-04 16:29:17 +0300

    Written smart contract

---
+++ contracts/Depositer.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Depositer{
+    mapping(address => uint256) balances;
+    
+    event Deposit(uint value, address depositer);
+    event Withdraw(uint value, address withdrawer);
+
+    function deposit() public payable{
+        balances[msg.sender]+= msg.value;
+
+        emit Deposit(msg.value, msg.sender);
+    }
+
+
+    function withdraw () public payable{
+        uint256 userBalance=balances[msg.sender];
+        balances[msg.sender]=0;
+        payable(msg.sender).transfer(userBalance);  
+
+        emit Withdraw(userBalance, msg.sender);
+        
+    }
+}
