77eb7097fec967f46e76c28a5912339f28088a7b
Andy Johnson <andreysima6@gmail.com>
2023-07-05 00:24:21 +0300

    Written ERC20 Contract

---
+++ contracts/ERC20.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract ERC20{
+    address public owner;
+    string public name;
+    string public symbol;
+    mapping (address=>uint256) balances;
+    
+    constructor(address _owner, string memory _name, string memory _symbol){
+        owner=_owner;
+        name=_name;
+        symbol=_symbol;
+        balances[owner]=1000;
+    }
+
+    function transfer(address receiver, uint256 amount)public{
+        require(amount<=balances[msg.sender], "Don`t have tokens for this transaction!");
+        balances[msg.sender]-=amount;
+        balances[receiver]+=amount;
+        
+    }
+    
+
+
+    
+
+    function getName()public view returns(string memory){
+        return name;
+    }
+}
