f89350416c7246509de4bc28ce653cccf989b09e
Andy Johnson <andreysima6@gmail.com>
2023-07-05 16:36:44 +0300

    Added new function for voting

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -2,15 +2,26 @@
 pragma solidity ^0.8.9;
 
 contract ERC20{
-    address public owner;
-    string public name;
-    string public symbol;
+    address private owner;
+    string private name;
+    string private symbol;
+    uint256 private voteVoice;
+    bool isVotingStarted;
+    uint256 startTime;
+    uint256 finishTime;
+    uint256 votingDuration =180;
+    uint256 positiveVote;
+    uint256 negativeVote;
+    uint256 result;
+
     mapping (address=>uint256) balances;
-    
-    constructor(address _owner, string memory _name, string memory _symbol){
+    event StartVoting(uint256 startTime, uint256 finishTime, address voter);
+    event EndOfVoting(uint256 result);
+    constructor(address _owner, string memory _name, string memory _symbol, uint256 _voteVoice){
         owner=_owner;
         name=_name;
         symbol=_symbol;
+        voteVoice=_voteVoice;
         balances[owner]=1000;
     }
 
@@ -21,7 +32,59 @@ contract ERC20{
         
     }
     
+    function startVoting()public{
+        require(balances[msg.sender]<=0, "Don`t have tokens for start voting");
+        require(isVotingStarted==true, "Voting dont start");
+        startTime=block.timestamp;
+        finishTime=startTime+votingDuration;
+        
+        emit StartVoting(startTime,finishTime, msg.sender);
 
+    }
+
+    function voting()public{
+        voteVoice=balances[msg.sender];
+        positiveVote+=voteVoice;
+        negativeVote+=voteVoice;
+    }
+
+    function endOfVoting()public{
+        if(positiveVote>negativeVote){
+            result=positiveVote;
+            emit EndOfVoting(positiveVote);
+        }
+        
+        else if(negativeVote>positiveVote){
+            result=negativeVote;
+            emit EndOfVoting(negativeVote);
+        }
+        else{
+            emit EndOfVoting(0);
+            result=0;
+        }
+        positiveVote=0;
+        negativeVote=0;   
+    }
+
+    function getResult()public view returns(uint256){
+        return result;
+    }
+
+    function getVote1()public view returns(uint256){
+        return positiveVote;
+    }
+
+    function getVote2()public view returns(uint256){
+        return negativeVote;
+    }
+
+    function getStartTime()public view returns(uint256){
+        return startTime;
+    }
+
+    function getFinishTime()public view returns(uint256){
+        return finishTime;
+    }
 
     function getBalance(address user)public view returns(uint256){
         return balances[user];
